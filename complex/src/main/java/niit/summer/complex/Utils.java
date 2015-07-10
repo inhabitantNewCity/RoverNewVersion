710e77a61211f5068b55c3bd59b5bea96d8f3264
Mikhail Chugunov <chugunov.ma@gmail.com>
2015-07-10 14:33:15 +0300

    Modify comparison - now using Double.compare. Add entries to .gitignore

--- complex/src/main/java/niit/summer/complex/Utils.java
+++ complex/src/main/java/niit/summer/complex/Utils.java
@@ -2,6 +2,6 @@ package niit.summer.complex;
 
 public class Utils {
     public static boolean isEqual(double a, double b) {
-        return Math.abs(a - b) < 0.0001;
+        return Double.compare(a, b) == 0;
     }
 }
